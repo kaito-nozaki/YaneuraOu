077395bee7000e81f2741350c56b7351b88d28af
nozaki <kaito.nozaki.tmu@gmail.com>
2021-12-26 14:53:34 +0900

    makefileとbuild.shの編集

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 cd source
 make
+
+# エンジンをbinフォルダに移動
+mkdir -p ../bin
+mv YaneuraOu-by-gcc ../bin/
+
+# 評価関数HalfKPE9をダウンロード, 解凍
+wget https://github.com/tttak/YaneuraOu/releases/download/V4.89_NNUE-HalfKPE9/NNUE-HalfKPE9.zip
+unzip NNUE-HalfKPE9.zip
+
+# 評価関数をevalフォルダに移動
+mkdir -p /content/YaneuraOu/bin/eval
+!cp -pr /content/NNUE-HalfKPE9/nn.bin /content/YaneuraOu/bin/eval
+
