e8e67597141681f80627f2c44bbd3f83692a9e48
nozaki <kaito.nozaki.tmu@gmail.com>
2021-12-26 14:58:21 +0900

    build.shの編集

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ make
 mkdir -p ../bin
 mv YaneuraOu-by-gcc ../bin/
 
+# 一旦contentへ
+cd /content
+
 # 評価関数HalfKPE9をダウンロード, 解凍
 wget https://github.com/tttak/YaneuraOu/releases/download/V4.89_NNUE-HalfKPE9/NNUE-HalfKPE9.zip
 unzip NNUE-HalfKPE9.zip
 
 # 評価関数をevalフォルダに移動
 mkdir -p /content/YaneuraOu/bin/eval
-!cp -pr /content/NNUE-HalfKPE9/nn.bin /content/YaneuraOu/bin/eval
+cp -pr /content/NNUE-HalfKPE9/nn.bin /content/YaneuraOu/bin/eval
 
